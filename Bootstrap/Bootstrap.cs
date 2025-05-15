477a41102928bd25a2bdf94c150b86f375e54d5b
skye rogers <skyero@amazon.com>
2025-05-15 11:58:08 -0700

    remove retry strategy

--- Bootstrap/Bootstrap.cs
+++ Bootstrap/Bootstrap.cs
@@ -80,7 +80,7 @@ namespace Amazon.Kinesis.ClientLibrary.Bootstrap
                     {
                         throw new Exception($"Failed to download {FileName} after {maxRetries} attempts: {ex.Message}", ex);
                     }
-                }
+
             }
         }
 
