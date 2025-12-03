3d632f91ec1fcd3e633760d155d33a201a712bed
skye rogers <skyero@amazon.com>
2025-12-03 10:56:32 -0800

    robust clean up strategy and added dedicated sample.properties file

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -7,27 +7,23 @@ dotnet add Bootstrap/Bootstrap.csproj reference ClientLibrary/ClientLibrary.cspr
 dotnet build SampleConsumer/SampleConsumer.csproj -o SampleConsumer/bin
 dotnet build Bootstrap/Bootstrap.csproj -o SampleConsumer/bin
 
+SAMPLE_PROPERTIES=".github/resources/kcl.properties"
+
 # Manipulate sample.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
 # Depending on the OS, different properties need to be changed
 if [[ "$RUNNER_OS" == "macOS" ]]; then
-  sed -i "" "s/kclnetsample/$STREAM_NAME/g" SampleConsumer/kcl.properties
-  sed -i "" "s/DotNetKinesisSample/$APP_NAME/g" SampleConsumer/kcl.properties
-  sed -i "" 's/us-east-5/us-east-1/g' SampleConsumer/kcl.properties
-  sed -i "" "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/SampleConsumer.dll|g" SampleConsumer/kcl.properties
-  grep -v "idleTimeBetweenReadsInMillis" SampleConsumer/kcl.properties > SampleConsumer/temp.properties
-  echo "idleTimeBetweenReadsInMillis = 250" >> SampleConsumer/temp.properties
-  mv SampleConsumer/temp.properties SampleConsumer/kcl.properties
+  sed -i "" "s/kclnetsample/$STREAM_NAME/g" $SAMPLE_PROPERTIES
+  sed -i "" "s/DotNetKinesisSample/$APP_NAME/g" $SAMPLE_PROPERTIES
+  sed -i "" "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/SampleConsumer.dll|g" $SAMPLE_PROPERTIES
   sed -i "" "51s/kclnetsample/$STREAM_NAME/g" SampleProducer/SampleProducer.cs
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  sed -i "s/kclnetsample/$STREAM_NAME/g" SampleConsumer/kcl.properties
-  sed -i "s/DotNetKinesisSample/$APP_NAME/g" SampleConsumer/kcl.properties
-  sed -i 's/us-east-5/us-east-1/g' SampleConsumer/kcl.properties
-  sed -i "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/SampleConsumer.dll|g" SampleConsumer/kcl.properties
-  sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" SampleConsumer/kcl.properties
+  sed -i "s/kclnetsample/$STREAM_NAME/g" $SAMPLE_PROPERTIES
+  sed -i "s/DotNetKinesisSample/$APP_NAME/g" $SAMPLE_PROPERTIES
+  sed -i "s|executableName = dotnet SampleConsumer.dll|executableName = dotnet bin/SampleConsumer.dll|g" $SAMPLE_PROPERTIES
   sed -i "51s/kclnetsample/$STREAM_NAME/g" SampleProducer/SampleProducer.cs
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
 fi
 
-cat SampleConsumer/kcl.properties
+cat $SAMPLE_PROPERTIES
--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -2,18 +2,13 @@
 set -e
 set -o pipefail
 
-# Get records from stream to verify they exist before continuing
-SHARD_ITERATOR=$(aws kinesis get-shard-iterator --stream-name $STREAM_NAME --shard-id shardId-000000000000 --shard-iterator-type TRIM_HORIZON --query 'ShardIterator' --output text)
-INITIAL_RECORDS=$(aws kinesis get-records --shard-iterator $SHARD_ITERATOR)
-RECORD_COUNT_BEFORE=$(echo $INITIAL_RECORDS | jq '.Records | length')
-
-echo "Found $RECORD_COUNT_BEFORE records in stream before KCL start"
+SAMPLE_PROPERTIES="../.github/resources/kcl.properties"
 
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
-  (cd SampleConsumer && gtimeout $RUN_TIME_SECONDS dotnet run --project ../Bootstrap/Bootstrap.csproj --properties kcl.properties --execute 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
+  (cd SampleConsumer && gtimeout $RUN_TIME_SECONDS dotnet run --project ../Bootstrap/Bootstrap.csproj --properties $SAMPLE_PROPERTIES --execute 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  (cd SampleConsumer && timeout $RUN_TIME_SECONDS dotnet run --project ../Bootstrap/Bootstrap.csproj --properties kcl.properties --execute 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
+  (cd SampleConsumer && timeout $RUN_TIME_SECONDS dotnet run --project ../Bootstrap/Bootstrap.csproj --properties $SAMPLE_PROPERTIES --execute 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
