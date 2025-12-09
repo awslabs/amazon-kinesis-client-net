fe11ba0f2c647d5a5234cb74b9f7225332e7b5e7
skyero-aws <skyero@amazon.com>
2025-12-09 09:35:11 -0800

    Add github workflow for KCL sample run testing (#290)

---
+++ .github/scripts/manipulate_properties.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Build the Bootstrap project and add project references
+dotnet add Bootstrap/Bootstrap.csproj reference SampleConsumer/SampleConsumer.csproj
+dotnet add Bootstrap/Bootstrap.csproj reference ClientLibrary/ClientLibrary.csproj
+dotnet build SampleConsumer/SampleConsumer.csproj -o SampleConsumer/bin
+dotnet build Bootstrap/Bootstrap.csproj -o SampleConsumer/bin
+
+SAMPLE_PROPERTIES=".github/resources/github_workflow.properties"
+
+# Manipulate sample.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
+# Depending on the OS, different properties need to be changed
+if [[ "$RUNNER_OS" == "macOS" ]]; then
+  sed -i "" "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" $SAMPLE_PROPERTIES
+  sed -i "" "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" $SAMPLE_PROPERTIES
+  sed -i "" "s/EXECUTABLE_NAME_PLACEHOLDER/dotnet bin\/SampleConsumer.dll/g" $SAMPLE_PROPERTIES
+  sed -i "" "51s/kclnetsample/$STREAM_NAME/g" SampleProducer/SampleProducer.cs
+elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
+  sed -i "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" $SAMPLE_PROPERTIES
+  sed -i "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" $SAMPLE_PROPERTIES
+  sed -i "s/EXECUTABLE_NAME_PLACEHOLDER/dotnet bin\/SampleConsumer.dll/g" $SAMPLE_PROPERTIES
+  sed -i "51s/kclnetsample/$STREAM_NAME/g" SampleProducer/SampleProducer.cs
+else
+  echo "Unknown OS: $RUNNER_OS"
+  exit 1
+fi
+
+cat $SAMPLE_PROPERTIES
---
+++ .github/scripts/start_kcl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+SAMPLE_PROPERTIES="../.github/resources/github_workflow.properties"
+
+if [[ "$RUNNER_OS" == "macOS" ]]; then
+  brew install coreutils
+  (cd SampleConsumer && gtimeout $RUN_TIME_SECONDS dotnet run --project ../Bootstrap/Bootstrap.csproj --properties $SAMPLE_PROPERTIES --execute 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
+elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
+  (cd SampleConsumer && timeout $RUN_TIME_SECONDS dotnet run --project ../Bootstrap/Bootstrap.csproj --properties $SAMPLE_PROPERTIES --execute 2>&1 | tee ../kcl_output.log) || [ $? -eq 124 ]
+else
+  echo "Unknown OS: $RUNNER_OS"
+  exit 1
+fi
+
+echo "---------ERROR LOGS HERE-------"
+grep -i error kcl_output.log || echo "No errors found in logs"
